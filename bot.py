90b14a78f9eff84d3e6272a2c3c2b8605cd077a2
lvillarr <luciano.villarroel.parra@gmail.com>
2026-04-10 18:40:35 -0400

    actualiza mensaje de bienvenida

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 
 async def start(update, context):
-    await update.message.reply_text("Hola! Soy el bot de Arauco MC")
+    await update.message.reply_text("Hola! Soy el bot de MC")
 
 app = ApplicationBuilder().token(os.environ["TELEGRAM_TOKEN"]).build()
 app.add_handler(CommandHandler("start", start))
